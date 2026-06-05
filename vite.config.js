e3725925fae28e5bd9dc3ff39ed30467bbf4d7f6
Parker Sheldon <113317341+prkrshldn@users.noreply.github.com>
2026-06-05 14:09:42 -0400

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,6 @@ import { defineConfig } from 'vite'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
-  base: '/rotten-apples/', // I'm most defo on the spectrum
   plugins: [
     tailwindcss(),
   ],
